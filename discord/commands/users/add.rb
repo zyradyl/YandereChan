96eeabaaf72cbe46de22fb5a905b0a4667bf5b07
Zyradyl <nmspencer@icloud.com>
2020-11-01 15:08:07 -0600

    users/add.rb is actually done!

--- discord/commands/users/add.rb
+++ discord/commands/users/add.rb
@@ -8,37 +8,67 @@ module UsersAdd
   include CheckPermissions
   include Logging
 
-  def self.process(event, rank)
-    return unless CheckPermissions.process(event) <= 1
-
-    case rank
-    when 'senpai'
-      add_senpai(event)
-    when 'rival'
-      add_rival(event)
-    when 'kouhai'
-      add_kouhai(event)
+  def self.add_kouhai(event)
+    event.user.await(:answer) do |answer_event|
+      case answer_event.message.content.to_s.downcase
+      when 'yes'
+        send_embed(answer_event, kouhai_response_yes)
+        answer_event.respond('**COMMAND ERROR:** Unknown Error.')
+        true
+      when 'no'
+        send_embed(answer_event, kouhai_response_no)
+        answer_event.respond('**COMMAND ABORTED.**')
+        true
+      else
+        if answer_event.channel.id == event.channel.id
+          YANDERE.send_message(answer_event.channel.id, CONFIG['logging']['users']['add']['kouhai']['error'])
+        else
+          YANDERE.send_message(answer_event.channel.id, CONFIG['logging']['users']['add']['kouhai']['avoid'])
+        end
+        false
+      end
     end
-  end
-
-  def self.add_senpai(event)
-    users = event.message.mentions
-    file = "#{CONFIG['bot']['storage']}#{event.server.id}/users/senpais.txt"
-
-    users.each do |user|
-      File.open(file, 'a') { |f| f.write("#{user.id}\n") }
-    end
-    Logging.users(event, 'add', 'senpai')
+    send_embed(event, kouhai_embed)
   end
 
   def self.add_rival(event)
-    users = event.message.mentions
     file = "#{CONFIG['bot']['storage']}#{event.server.id}/users/rivals.txt"
+    adds = check_dups(event)
+    return if adds.empty?
 
-    users.each do |user|
+    adds.each do |user|
       File.open(file, 'a') { |f| f.write("#{user.id}\n") }
     end
     Logging.users(event, 'add', 'rival')
+    event.respond(CONFIG['messages']['commands']['users']['add']['rival'])
+  end
+
+  def self.add_senpai(event)
+    file = "#{CONFIG['bot']['storage']}#{event.server.id}/users/senpais.txt"
+    adds = check_dups(event)
+    return if adds.empty?
+
+    adds.each do |user|
+      File.open(file, 'a') { |f| f.write("#{user.id}\n") }
+    end
+    Logging.users(event, 'add', 'senpai')
+    event.respond(CONFIG['messages']['commands']['users']['add']['senpai'])
+  end
+
+  def self.check_dups(event)
+    rivals = File.readlines("#{CONFIG['bot']['storage']}/#{event.server.id}/users/rivals.txt")
+    senpais = File.readlines("#{CONFIG['bot']['storage']}/#{event.server.id}/users/senpais.txt")
+    users = event.message.mentions
+    users.each do |user|
+      if rivals.include?("#{user.id}\n")
+        event.respond("<@#{user.id}> #{CONFIG['messages']['commands']['users']['add']['dup_rival']}")
+        users.delete(user)
+      elsif senpais.include?("#{user.id}\n")
+        event.respond("<@#{user.id}> #{CONFIG['messages']['commands']['users']['add']['dup_senpai']}")
+        users.delete(user)
+      end
+    end
+    users
   end
 
   def self.kouhai_embed
@@ -64,16 +94,6 @@ module UsersAdd
     embed
   end
 
-  def self.kouhai_response_yes
-    embed = kouhai_response
-    embed.title = CONFIG['logging']['users']['add']['kouhai']['yes']['title']
-    embed.description = CONFIG['logging']['users']['add']['kouhai']['yes']['desc']
-    embed.image = Discordrb::Webhooks::EmbedImage.new(
-      url: CONFIG['logging']['users']['add']['kouhai']['yes']['image']
-    )
-    embed
-  end
-
   def self.kouhai_response_no
     embed = kouhai_response
     embed.title = CONFIG['logging']['users']['add']['kouhai']['no']['title']
@@ -84,6 +104,29 @@ module UsersAdd
     embed
   end
 
+  def self.kouhai_response_yes
+    embed = kouhai_response
+    embed.title = CONFIG['logging']['users']['add']['kouhai']['yes']['title']
+    embed.description = CONFIG['logging']['users']['add']['kouhai']['yes']['desc']
+    embed.image = Discordrb::Webhooks::EmbedImage.new(
+      url: CONFIG['logging']['users']['add']['kouhai']['yes']['image']
+    )
+    embed
+  end
+
+  def self.process(event, rank)
+    return unless CheckPermissions.process(event) <= 1
+
+    case rank
+    when 'senpai'
+      add_senpai(event)
+    when 'rival'
+      add_rival(event)
+    when 'kouhai'
+      add_kouhai(event)
+    end
+  end
+
   def self.send_embed(event, embed)
     YANDERE.send_message(
       event.channel.id,
@@ -91,29 +134,5 @@ module UsersAdd
       false,
       embed
     )
-  end
-
-  def self.add_kouhai(event)
-    event.user.await(:answer) do |answer_event|
-      case answer_event.message.content.to_s.downcase
-      when 'yes'
-        send_embed(answer_event, kouhai_response_yes)
-        answer_event.respond('**COMMAND ERROR:** Unknown Error.')
-        true
-      when 'no'
-        send_embed(answer_event, kouhai_response_no)
-        answer_event.respond('**COMMAND ABORTED.**')
-        true
-      else
-        if answer_event.channel.id == event.channel.id
-          YANDERE.send_message(answer_event.channel.id, CONFIG['logging']['users']['add']['kouhai']['error'])
-        else
-          YANDERE.send_message(answer_event.channel.id, CONFIG['logging']['users']['add']['kouhai']['avoid'])
-        end
-        false
-      end
-    end
-
-    send_embed(event, kouhai_embed)
   end
 end
